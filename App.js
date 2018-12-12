ad3f757de5d9c7e4117a27c53c1051f73943f44c
Victor R <victor.ribeiro.s@outlook.com>
2018-12-11 22:07:04 -0200

    Inicio

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+
+        <TextInput style={{borderColor:'black', borderWidth:1, width:30}} />
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
