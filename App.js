ab12b09e6387e34ed1d47eb64130bfe50c00aa8d
Victor R <victor.ribeiro.s@outlook.com>
2018-12-15 20:59:08 -0200

    Fim

--- App.js
+++ App.js
@@ -1,33 +1,100 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity, Dimensions} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 export default class App extends React.Component {
 
-  constructor(props) {
+  mathArray;
+    constructor(props) {
     super(props);
     this.state = {text: ''};
   }
-
+    
   _addNum = (n) => {
+    if(this.state.text[this.state.text.length-1] == "." && n == ".")
+    return;
+
     this.setState({text: this.state.text + n});
+  }
+
+  _isNumber = (num) => {
+    if(num != null)
+        return(num != '+' && num != '*' && num != '-' && num != '/'); 
+    return false;   
   }
 
   _addSign = (s) => {
     
-    if(this.state.text[this.state.text.length-1] != null)
-      if (this.state.text[this.state.text.length-1] != '+' && this.state.text[this.state.text.length-1] != '*' &&
-          this.state.text[this.state.text.length-1] != '-' && this.state.text[this.state.text.length-1] != '/' )
+    if(this._isNumber(this.state.text[this.state.text.length-1]))  
         this.setState({text: this.state.text + s}); //Caso tenha um número atrás, adicione o sinal na frente
-      else
-        this.setState({text: this._setCharAt(this.state.text,this.state.text.length-1,s)}); //Caso não seja um número, substitua o sinal antigo
+    else
+       this.setState({text: this._setCharAt(this.state.text,this.state.text.length-1,s)}); //Caso não seja um número, substitua o sinal antigo
   }
 
   _backspace() {
-    this.setState({text: this._setCharAt(this.state.text,this.state.text.length-1,'')});
+      this.setState({text: this._setCharAt(this.state.text,this.state.text.length-1,'')});
   }
 
   _calculate() {
-    //Essa calculadora não calcula ainda, desculpa.
+      mathString = this.state.text;
+      number = "";
+      j=0;
+
+      for(i=0;i < mathString.length;i++) { //Quebra a string do TextInput e coloca em um array.
+        for(;this._isNumber(mathString[i]);i++)
+          number += mathString[i];
+
+        mathArray[j] = {"number":parseFloat(number),
+                         "sign": mathString[i]};
+              
+        number = "";
+        j++;
+
+      }
+
+      //Resolve as equações no array, reduzindo ela até um único index.
+      for(;mathArray[1];) {
+
+        for (j=0; j < mathArray.length; j++) {
+
+          if(mathArray[j] != null)
+          switch(mathArray[j]["sign"]) { //Passa uma primeira vez no vetor para dar prioridade à multiplicação e divisão
+            case '/':  mathArray[j]["number"] /= mathArray[j+1]["number"];
+                       this._removeIndex(j+1);
+                       break; 
+
+             case '*':  mathArray[j]["number"] *= mathArray[j+1]["number"];
+                       this._removeIndex(j+1);
+                       break;
+          }      
+        }
+
+        for (j=0; j < mathArray.length; j++) {
+          if(mathArray[j] != null)
+
+          switch(mathArray[j]["sign"])
+          {
+              case '+':  mathArray[j]["number"] += mathArray[j+1]["number"];
+                         this._removeIndex(j+1);
+                         break;
+                       
+              case '-':  mathArray[j]["number"] -= mathArray[j+1]["number"];
+                         this._removeIndex(j+1);
+                         break;  
+          }
+      }
+    }
+      this.setState({text: mathArray[0]["number"].toString()});
+
+  }
+
+  _removeIndex = (i) => {
+    if(mathArray[i-1] != null)
+      mathArray[i-1]["sign"] = mathArray[i]["sign"];
+
+    for(;i < mathArray.length;i++)
+        mathArray[i] = mathArray[i+1];
+
+    mathArray[i] == null;
   }
 
   _clear = () => {
@@ -41,33 +108,44 @@ export default class App extends React.Component {
 
   render() {
 
+    mathArray = [{
+      "number": '0',
+      "sign":'+'
+    }];
+
+    
     return (
 
       <View style={{flex: 1}}>
+        <View style={styles.statusbar} />
+
         <TextInput style={styles.textinput} maxLength = {14} editable = {false} value={this.state.text} /> 
         
-        <View style={styles.buttoncontainer}> 
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(1)}><Text style={styles.containedbuttontext}>1</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(2)}><Text style={styles.containedbuttontext}>2</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(3)}><Text style={styles.containedbuttontext}>3</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addSign('+')}><Text style={styles.containedbuttontext}>+</Text></TouchableOpacity>
+        <View style={styles.buttoncontainer}>          
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._clear()}><Text style={[styles.containedbuttontext, styles.buttontexthighlight]}>AC</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._backspace() }><Text style={styles.containedbuttontext}>A</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addSign('*')}><Text style={styles.containedbuttontext}>*</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addSign('/')}><Text style={styles.containedbuttontext}>/</Text></TouchableOpacity>
 
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(4)}><Text style={styles.containedbuttontext}>4</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(5)}><Text style={styles.containedbuttontext}>5</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(6)}><Text style={styles.containedbuttontext}>6</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addSign('-')}><Text style={styles.containedbuttontext}>-</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(7)}><Text style={styles.containedbuttontext}>7</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(8)}><Text style={styles.containedbuttontext}>8</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(9)}><Text style={styles.containedbuttontext}>9</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addSign('+')}><Text style={styles.containedbuttontext}>+</Text></TouchableOpacity>
 
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(7)}><Text style={styles.containedbuttontext}>7</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(8)}><Text style={styles.containedbuttontext}>8</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(9)}><Text style={styles.containedbuttontext}>9</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addSign('*')}><Text style={styles.containedbuttontext}>*</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(4)}><Text style={styles.containedbuttontext}>4</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(5)}><Text style={styles.containedbuttontext}>5</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(6)}><Text style={styles.containedbuttontext}>6</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addSign('-')}><Text style={styles.containedbuttontext}>-</Text></TouchableOpacity>
 
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addNum(0)}><Text style={styles.containedbuttontext}>0</Text></TouchableOpacity>
-
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._clear()}><Text style={styles.containedbuttontext}>C</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._addSign('/')}><Text style={styles.containedbuttontext}>/</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._calculate() }><Text style={styles.containedbuttontext}>=</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.containedbutton} onPress={() => this._backspace() }><Text style={styles.containedbuttontext}>Apagar</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(1)}><Text style={styles.containedbuttontext}>1</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(2)}><Text style={styles.containedbuttontext}>2</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(3)}><Text style={styles.containedbuttontext}>3</Text></TouchableOpacity>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum('.')}><Text style={styles.containedbuttontext}>.</Text></TouchableOpacity>
+          
+          <View style={styles.containedbutton}></View>
+          <TouchableOpacity ref="touch" style={styles.containedbutton} onPress={() => this._addNum(0)}><Text style={styles.containedbuttontext}>0</Text></TouchableOpacity>
+          <View style={styles.containedbutton}></View>
+          <TouchableOpacity ref="touch" style={[styles.containedbutton, styles.buttonequals]} onPress={() => this._calculate() }><Text style={[styles.containedbuttontext, styles.buttonequals]}>=</Text></TouchableOpacity>
 
         </View> 
       </View>
@@ -78,28 +156,32 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   
+  statusbar: {
+    height:Expo.Constants.statusBarHeight,
+    backgroundColor: 'black'
+  },
+
   textinput: {
      flex: 1,
-     
-     backgroundColor: 'white',
-     borderColor:'#333333',
 
-     fontWeight: 'bold',
      fontSize: 40,
 
-     borderTopWidth: 25,
-     borderBottomWidth: 5,
-     borderLeftWidth: 5,
-     borderRightWidth: 5,
+     fontFamily: 'monospace',
+     textAlignVertical: 'bottom',
+     textAlign: 'right',
+     
+     backgroundColor: 'white',
+     borderColor:'black',
 
-     paddingHorizontal: 10
+     borderBottomWidth: 0.5,
+     paddingHorizontal: 10,
+     paddingVertical:10
   },
 
   buttoncontainer: {
     flex: 3,
 
-
-    backgroundColor: 'grey',
+    backgroundColor: 'black',
     flexDirection: 'row',
     flexWrap:'wrap',
     justifyContent:'space-evenly'
@@ -108,19 +190,27 @@ const styles = StyleSheet.create({
   containedbutton: {
 
     flex:1,
-    margin:5,
+    margin:0.5,
     flexBasis:'22%',
     aspectRatio: 1,
     justifyContent: 'center',
-    borderRadius: 3,
 
     backgroundColor:'white'
   },
 
   containedbuttontext: {
-    fontWeight: 'bold',
-    fontSize: 30,
+    fontFamily: 'Roboto',
+    fontSize: 28,
     textAlign: 'center'
+  },
+
+  buttonequals: {
+    backgroundColor:'#f97c00',
+    color:'#ffffff'
+  },
+
+  buttontexthighlight: {
+    color:'#f97c00'
   }
 
 });
